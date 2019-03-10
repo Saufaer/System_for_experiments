f6a257f03603c6a7720b935caeea41ea48db066e
Oleg <ogretoleg@yandex.ru>
2019-03-10 16:46:43 +0300

    add start GUI

---
+++ Bridge/Bridge/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Bridge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(472, 76);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(141, 91);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "CreateFile";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(471, 208);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(150, 93);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "Write";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(470, 331);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(150, 94);
+            this.metroButton3.TabIndex = 2;
+            this.metroButton3.Text = "Read";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(467, 453);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(153, 95);
+            this.metroButton4.TabIndex = 3;
+            this.metroButton4.Text = "Delete";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Location = new System.Drawing.Point(743, 123);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(150, 85);
+            this.metroButton5.TabIndex = 4;
+            this.metroButton5.Text = "Copy";
+            this.metroButton5.UseSelectable = true;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.Location = new System.Drawing.Point(740, 269);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(152, 78);
+            this.metroButton6.TabIndex = 5;
+            this.metroButton6.Text = "Move";
+            this.metroButton6.UseSelectable = true;
+            this.metroButton6.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 622);
+            this.Controls.Add(this.metroButton6);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private MetroFramework.Controls.MetroButton metroButton6;
+    }
+}
+
---
+++ Bridge/Bridge/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bridge
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //file location
+        string fileLoc = Environment.CurrentDirectory + "/" + "config.xml";
+
+        //Create
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = null;
+            if (!File.Exists(fileLoc))
+            {
+                using (fs = File.Create(fileLoc))
+                {
+
+                }
+            }
+        }
+
+        //Write
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(fileLoc))
+            {
+                using (StreamWriter sw = new StreamWriter(fileLoc))
+                {
+                    string start = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+                    string one = " <par1>examin.exe</par1>";
+                    string two = " <par2>-lib</par2>";
+                    string three = " <par3>stronginc3.dll</par3>";
+                    string end = "<?include somedata?>";
+                    sw.Write(start + "\n<exe>\n"+one+"\n"+two+" \n"+ three+"\n</exe>\n"+end);
+                }
+            }
+        }
+
+        //Read
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(fileLoc))
+            {
+                using (TextReader tr = new StreamReader(fileLoc))
+                {
+                    MessageBox.Show(tr.ReadLine());
+                }
+            }
+        }
+
+        //Delete
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(fileLoc))
+            {
+                File.Delete(fileLoc);
+            }
+        }
+
+        //Copy
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            string fileLocCopy = Environment.CurrentDirectory + "/" + "config.xml"; ;
+            if (File.Exists(fileLoc))
+            {
+                if (File.Exists(fileLocCopy))
+                    File.Delete(fileLocCopy);
+                File.Copy(fileLoc, fileLocCopy);
+            }
+        }
+
+        //Move
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            string fileLocMove = Environment.CurrentDirectory + "/" + "config" + System.DateTime.Now.Ticks + ".xml";
+            if (File.Exists(fileLoc))
+            {
+                File.Move(fileLoc, fileLocMove);
+            }
+        }
+    }
+}
