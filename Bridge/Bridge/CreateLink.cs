06d051637c670070c9668436ccc287da5acd4c9e
Oleg <ogretoleg@yandex.ru>
2019-03-19 18:49:10 +0300

    move info to alone file

--- Bridge/Bridge/CreateLink.cs
+++ Bridge/Bridge/CreateLink.cs
@@ -13,6 +13,7 @@ namespace Bridge
     public partial class CreateLink : MetroFramework.Forms.MetroForm
     {
         int size = 9;
+        Info InfoData = new Info();
         public CreateLink()
         {
             InitializeComponent();
@@ -24,58 +25,11 @@ namespace Bridge
 
             for (int i=0;i < size;i++)
             {
-                InfoTable.Rows.Add(ParameterArr[i], ValidValuesArr[i], DefaultValuesArr[i], DescriptionArr[i]); }
+                InfoTable.Rows.Add(InfoData.ParameterArr[i], InfoData.ValidValuesArr[i], InfoData.DefaultValuesArr[i], InfoData.DescriptionArr[i]); }
         }
         string parameter;
         string value;
 
-
-        string[] ParameterArr = new string[9]
-        {
-"-NumPoints",
-"-StepPrintMessages",
-"-TypeMethod",
-"-TypeCalculation",
-"-TypeProcess",
-"-NumThread",
-"-SizeInBlock",
-"-IsPrintFile",
-"-Dimension"
-        };
-        string[] ValidValuesArr = new string[9]
-        {
-">0                                                      ",
-">0                                                      ",
-"перечисление ETypeMethod \r\n или число от 0 до 8       ",
-"перечисление ETypeCalculation \r\n или число от 0 до 2  ",
-"перечисление ETypeProcess \r\n или значение от 0 до 2   ",
-">0                                                      ",
-">0                                                      ",
-"0|1 или false|true                                      ",
-">0                                                      "
-        };
-        string[] DefaultValuesArr = new string[9] {
-"1",
-"1000",
-"StandartMethod",
-"OMP",
-"SynchronousProces",
-"1",
-"32",
-"false",
-"берется из задачи"
-        };
-        string[] DescriptionArr = new string[9] {
-   " число точек испытания, порождаемых \r\n методом за одну итерацию                          ",
-   " через сколько итераций печатать \r\n в консоли сообщение о текущем количестве итераций    ",
-   " тип используемого метода                                                                  ",
-   " тип вычислительного ресурса,\r\n используемого для проведения испытаний                   ",
-   " тип процесса                                                                              ",
-   " число используемых потоков                                                                ",
-   " размер CUDA блока                                                                         ",
-   " печатать отчет в файл                                                                     ",
-   " размерность исходной задачи                                                               "
-   };
         System.Windows.Forms.Form f = System.Windows.Forms.Application.OpenForms["Form1"];
 
         
---
+++ Bridge/Bridge/Info.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Info
+    {
+        public string[] ParameterArr;
+        public string[] ValidValuesArr;
+        public string[] DefaultValuesArr;
+        public string[] DescriptionArr;
+        public Info()
+        {
+            ParameterArr = new string[9]
+     {
+"-NumPoints",
+"-StepPrintMessages",
+"-TypeMethod",
+"-TypeCalculation",
+"-TypeProcess",
+"-NumThread",
+"-SizeInBlock",
+"-IsPrintFile",
+"-Dimension"
+     };
+            ValidValuesArr = new string[9]
+            {
+">0",
+">0",
+"перечисление ETypeMethod \r\n или число от 0 до 8",
+"перечисление ETypeCalculation \r\n или число от 0 до 2",
+"перечисление ETypeProcess \r\n или значение от 0 до 2",
+">0",
+">0",
+"0|1 или false|true",
+">0"
+            };
+            DefaultValuesArr = new string[9] {
+"1",
+"1000",
+"StandartMethod",
+"OMP",
+"SynchronousProces",
+"1",
+"32",
+"false",
+"берется из задачи"
+        };
+            DescriptionArr = new string[9] {
+   " число точек испытания, порождаемых \r\n методом за одну итерацию",
+   " через сколько итераций печатать \r\n в консоли сообщение о текущем количестве итераций",
+   " тип используемого метода",
+   " тип вычислительного ресурса,\r\n используемого для проведения испытаний",
+   " тип процесса",
+   " число используемых потоков",
+   " размер CUDA блока",
+   " печатать отчет в файл",
+   " размерность исходной задачи"
+   };
+        }
+    }
+}
